9baf93d3cd7dea658a57380e3d6bd5db238f3e7b
Maciej0programista <143076811+Maciej0programista@users.noreply.github.com>
2024-08-08 13:09:07 +0300

    Update logic_lua.h

--- logic_lua.h
+++ logic_lua.h
@@ -4,7 +4,9 @@
 void setupLua();
 void loopLua();
 void openLuaScript(const char* filename);
-void installLuaScripts();
+void installLuaApps();
 bool createAppFolder();
+void setKeyFunction(char key, const char* functionName);
+void setAppForButton(char key, const char* appName);
 
 #endif
